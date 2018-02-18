d65c429273318f7e534ffbd60bc632c389a3a3d6
curious725 <curious725@gmail.com>
2018-02-18 19:59:44 +0200

    register ci_projects app

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -51,6 +51,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ci_projects.apps.CiProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
