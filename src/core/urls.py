88592ce4bee32f9b5097100f5a30eb165d7e2851
curious725 <curious725@gmail.com>
2018-02-19 10:27:36 +0200

    fix typing error in url pattern

--- src/core/urls.py
+++ src/core/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from ci_projects import views
 
 urlpatterns = [
-    url(r'&$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
 
     url(r'^ci/', include('ci_projects.urls')),
     url(r'^admin/', admin.site.urls),
