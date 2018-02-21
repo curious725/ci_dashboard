62f783ff0b9b620fda81b85fbf2c5e5663be7e32
curious725 <curious725@gmail.com>
2018-02-21 04:31:56 +0200

    add Build model

--- src/ci_projects/models.py
+++ src/ci_projects/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Build(models.Model):
+    session_id = models.IntegerField(primary_key=True)
+    started_by = models.EmailField()
+    created_at = models.DateTimeField()
+    summary_status = models.CharField(max_length=100)
+    duration = models.FloatField()
+    worker_time = models.FloatField()
+    bundle_time = models.FloatField()
+    num_workers = models.IntegerField()
+    branch = models.CharField(max_length=100)
+    commit_id = models.CharField(max_length=255)
+    started_tests_count = models.IntegerField()
+    passed_tests_count = models.IntegerField()
+    failed_tests_count = models.IntegerField()
+    pending_tests_count = models.IntegerField()
+    skipped_tests_count = models.IntegerField()
+    error_tests_count = models.IntegerField()
