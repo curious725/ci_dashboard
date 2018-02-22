bf5caba74b81e7adcc3e9e0e07e003450cf56a55
curious725 <curious725@gmail.com>
2018-02-22 14:07:21 +0200

    add stacked bar chart

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,78 +1,139 @@
-var endpoint = 'http://localhost:8000/ci/data/stacked-bar'
-$.ajax({
+var endpoint = 'http://localhost:8000/ci/data/';
+var builds = $.ajax({
   method: "GET",
   url: endpoint,
   success: function(data){
-    console.log(data);
+    finalData = prepare_data_stacked_bar(data);
+
+    passed = getPassed(finalData);
+    failed = getFailed(finalData);
+    dates = getDates(finalData);
+    createStackedBarChart(passed, failed, dates);
   },
   error: function(error_data){
     console.log("error");
     console.log(error_data);
-  }-
+  }
 });
 
+var prepare_data_stacked_bar = function(initialData){
+  console.log(initialData);
+  var finalData = {};
+  initialData.builds.forEach(function(element){
+    var date = element.created_at.slice(0,10);
+    if (!(date in finalData)) {
+      finalData[date] = {
+        passed: 0,
+        failed: 0
+      }
+    }
+    var status = element.summary_status;
+    if (status === 'passed') {
+      finalData[date].passed++;
+    } else if (status === 'failed') {
+      finalData[date].failed++;
+    }
+  })
 
-//  // Return with commas in between
-//   var numberWithCommas = function(x) {
-//     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-//   };
+  var keys = [];
+  for (var item in finalData) {
+    keys.push(item);
+  }
+  keys.sort();
 
-// var dataPack1 = [21000, 22000, 26000, 35000, 55000, 55000, 56000, 59000, 60000, 61000, 60100, 62000];
-// var dataPack2 = [1000, 1200, 1300, 1400, 1060, 2030, 2070, 4000, 4100, 4020, 4030, 4050];
-// var dates = ["May 1", "May 2", "May 3", "May 4", "May 5", "May 6",
-//            "May 7", "May 8", "May 9", "May 10", "May 11", "May 12"];
+  var sortedFinalData = {};
+  keys.forEach(function(element){
+    sortedFinalData[element] = finalData[element];
+  })
 
-// // Chart.defaults.global.elements.rectangle.backgroundColor = '#FF0000';
+  return sortedFinalData;
+};
 
-// var bar_ctx = document.getElementById('bar-chart');
-// var bar_chart = new Chart(bar_ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: dates,
-//         datasets: [
-//         {
-//             label: 'Bowser',
-//             data: dataPack1,
-//             backgroundColor: "rgba(55, 160, 225, 0.7)",
-//             hoverBackgroundColor: "rgba(55, 160, 225, 0.7)",
-//             hoverBorderWidth: 2,
-//             hoverBorderColor: 'lightgrey'
-//         },
-//         {
-//             label: 'Mario',
-//             data: dataPack2,
-//             backgroundColor: "rgba(225, 58, 55, 0.7)",
-//             hoverBackgroundColor: "rgba(225, 58, 55, 0.7)",
-//             hoverBorderWidth: 2,
-//             hoverBorderColor: 'lightgrey'
-//         },
-//         ]
-//     },
-//     options: {
-//         animation: {
-//           duration: 10,
-//         },
-//         tooltips: {
-//           mode: 'label',
-//           callbacks: {
-//           label: function(tooltipItem, data) {
-//             return data.datasets[tooltipItem.datasetIndex].label + ": " + numberWithCommas(tooltipItem.yLabel);
-//           }
-//           }
-//          },
-//         scales: {
-//           xAxes: [{
-//             stacked: true,
-//             gridLines: { display: false },
-//             }],
-//           yAxes: [{
-//             stacked: true,
-//             ticks: {
-//               callback: function(value) { return numberWithCommas(value); },
-//             },
-//             }],
-//         }, // scales
-//         legend: {display: true}
-//     } // options
-//    }
-// );
+var getDates = function(finalData) {
+  var dates = [];
+  for (var element in finalData){
+    dates.push(element)
+  }
+  return dates;
+}
+
+var getPassed = function(finalData) {
+  var passed = [];
+  for (var element in finalData) {
+    passed.push(finalData[element].passed);
+  }
+  console.log(passed);
+  return passed;
+}
+
+var getFailed = function(finalData) {
+  var failed = [];
+  for (var element in finalData) {
+    failed.push(finalData[element].failed);
+  }
+  return failed;
+}
+
+
+var createStackedBarChart = function(dataPack1, dataPack2,dates) {
+
+  var bar_ctx = document.getElementById('stacked-bar-chart');
+  var bar_chart = new Chart(bar_ctx, {
+      type: 'bar',
+      data: {
+          labels: dates,
+          datasets: [
+          {
+              label: 'passed',
+              data: dataPack1,
+              backgroundColor: "rgba(55, 160, 225, 0.7)",
+              hoverBackgroundColor: "rgba(55, 160, 225, 0.7)",
+              hoverBorderWidth: 2,
+              hoverBorderColor: 'lightgrey'
+          },
+          {
+              label: 'failed',
+              data: dataPack2,
+              backgroundColor: "rgba(225, 58, 55, 0.7)",
+              hoverBackgroundColor: "rgba(225, 58, 55, 0.7)",
+              hoverBorderWidth: 2,
+              hoverBorderColor: 'lightgrey'
+          },
+          ]
+      },
+      options: {
+          animation: {
+            duration: 10,
+          },
+          tooltips: {
+            mode: 'label',
+            callbacks: {
+            label: function(tooltipItem, data) {
+              return data.datasets[tooltipItem.datasetIndex].label + ": " + tooltipItem.yLabel;
+            }
+            }
+           },
+          scales: {
+            xAxes: [{
+              stacked: true,
+              gridLines: { display: false },
+              }],
+            yAxes: [{
+              stacked: true,
+              ticks: {
+                callback: function(value) { return value; },
+              },
+              }],
+          }, // scales
+          legend: {
+            display: true,
+            // position: 'right',
+          },
+          title: {
+            display: true,
+            text: 'Summary statuses for builds per day'
+          }
+      } // options
+     }
+  );
+};
