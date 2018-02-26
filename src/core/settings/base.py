c595ea8f54c284c5ab2d843e00721b3ba70c2dba
curious725 <curious725@gmail.com>
2018-02-27 01:44:53 +0200

    install and register django-rest-framework

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -52,6 +52,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ci_projects.apps.CiProjectsConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
